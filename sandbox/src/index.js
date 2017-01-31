bd04f798243d2a96c156a168a735d5b6ce94c540
Oleksiy Golovko <alexei.golovko@gmail.com>
2017-01-31 16:41:05 +0100

    initial commit

---
+++ sandbox/src/index.js
@@ -0,0 +1,17 @@
+
+/*
+ * This code is not intended to do anything useful or even run correctly
+ * The only purpose of it is testing the symboljs capabilities
+ */
+
+import lodash from 'lodash';
+const lodash = require('lodash');
+const {curryN: myCurryN} = require('lodash/fp');
+import * as lodash_ from 'lodash';
+import {fp} from lodash;
+
+const CONST = 'x';
+
+function lodash_import() {
+  return require('lodash')
+}
